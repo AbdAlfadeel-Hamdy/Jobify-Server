2569f1ab9db1191a15ac9e9a6f65a9fce4be61e2
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-08-31 18:25:00 +0300

    Add Hashing Password

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,14 +1,19 @@
 import { Handler } from "express";
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel";
+import { hashPassword } from "../utils/passwordUtils";
 
-export const login: Handler = async (req, res, next) => {
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
-};
 export const register: Handler = async (req, res, next) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
+
+  req.body.password = await hashPassword(req.body.password);
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ message: "User created." });
+};
+
+export const login: Handler = async (req, res, next) => {
+  const user = await User.findOne();
+  res.status(StatusCodes.OK).json({ user });
 };
--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -3,18 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = exports.login = void 0;
+exports.login = exports.register = void 0;
 const http_status_codes_1 = require("http-status-codes");
 const UserModel_1 = __importDefault(require("../models/UserModel"));
-const login = async (req, res, next) => {
-    const user = await UserModel_1.default.create(req.body);
-    res.status(http_status_codes_1.StatusCodes.CREATED).json({ user });
-};
-exports.login = login;
+const passwordUtils_1 = require("../utils/passwordUtils");
 const register = async (req, res, next) => {
     const isFirstAccount = (await UserModel_1.default.countDocuments()) === 0;
     req.body.role = isFirstAccount ? "admin" : "user";
+    req.body.password = await (0, passwordUtils_1.hashPassword)(req.body.password);
     const user = await UserModel_1.default.create(req.body);
-    res.status(http_status_codes_1.StatusCodes.CREATED).json({ user });
+    res.status(http_status_codes_1.StatusCodes.CREATED).json({ message: "User created." });
 };
 exports.register = register;
+const login = async (req, res, next) => {
+    const user = await UserModel_1.default.findOne();
+    res.status(http_status_codes_1.StatusCodes.OK).json({ user });
+};
+exports.login = login;
